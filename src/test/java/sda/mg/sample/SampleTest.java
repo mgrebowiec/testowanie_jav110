14f6283c8e838579ec0f8884d384b49b427475e7
mgrebowiec <michal.grebowiec@gmail.com>
2021-11-21 11:29:02 +0100

    przykładowe testy i wykorzystanie adnotacji

--- src/test/java/sda/mg/sample/SampleTest.java
+++ src/test/java/sda/mg/sample/SampleTest.java
@@ -1,11 +1,44 @@
 package sda.mg.sample;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
+
+    private static String sampleValue;
+
+    @BeforeAll
+    static void beforeAllTests() {
+        System.out.println("beforeAllTests");
+        sampleValue = "AAA";
+    }
+
+    @AfterAll
+    static void afterAllTests() {
+        System.out.println("afterAllTests");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("beforeEach test");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach test");
+    }
+
+    @Test
+    void shouldPrint1() {
+        System.out.println("test 1");
+        assertEquals(sampleValue, new String("AAA"));
+    }
+
+    @Test
+    void shouldPrint2() {
+        System.out.println("test 2");
+    }
 
     @Test
     void shouldCompareStringsSample() {
@@ -13,7 +46,23 @@ class SampleTest {
         String actualValue = "aa";
 
         //then
+//        assertAll(
+//                assertNotNull(actualValue),
+//                assertEquals(new String("aa"), actualValue)
+//        );
+
         assertNotNull(actualValue);
         assertEquals(new String("aa"), actualValue);
+//        assertSame(new String("aa"), actualValue);
+    }
+
+    // metoda niewidoczna dla JUnit (ponieważ nie została oznaczona adnotacją @Test)
+    void shouldTest() {
+        //given
+        String actualValue = "ab";
+
+        //then
+        assertNotNull(actualValue);
+        assertEquals(new String("ab"), actualValue);
     }
 }
