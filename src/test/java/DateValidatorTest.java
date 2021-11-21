a2927fef25e0f339c8599a741cb5eec8f43da5a2
mgrebowiec <michal.grebowiec@gmail.com>
2021-11-21 13:02:52 +0100

    DateValidator - pozostałe przypadki testowe

--- src/test/java/DateValidatorTest.java
+++ src/test/java/DateValidatorTest.java
@@ -17,4 +17,64 @@ class DateValidatorTest {
         //then
         assertFalse(result);
     }
+
+    @Test
+    void shouldReturnFalseForDateBeforeRange() {
+        //given
+        LocalDate sampleDate = LocalDate.of(2020, 12,30);
+
+        //when
+        boolean result = DateValidator.isValidDate(sampleDate);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnFalseForDateAfterRange() {
+        //given
+        LocalDate sampleDate = LocalDate.of(2021, 4, 2);
+
+        //when
+        boolean result = DateValidator.isValidDate(sampleDate);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    void shouldReturnTrueForMinDate() {
+        //given
+        LocalDate sampleDate = LocalDate.of(2021, 1, 1);
+
+        //when
+        boolean result = DateValidator.isValidDate(sampleDate);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnTrueForMaxDate() {
+        //given
+        LocalDate sampleDate = LocalDate.of(2021, 4, 1);
+
+        //when
+        boolean result = DateValidator.isValidDate(sampleDate);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    void shouldReturnTrueForDateInRange() {
+        //given
+        LocalDate sampleDate = LocalDate.of(2021, 2, 4);
+
+        //when
+        boolean result = DateValidator.isValidDate(sampleDate);
+
+        //then
+        assertTrue(result);
+    }
 }
